9a218e67db427b0beb6682a4eae028a476f5183e
01212 <marlonwiprud1@gmail.com>
2019-02-04 18:32:47 -0500

    set up base webpack config

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="test">hi</div>;
+  }
+}
+
+export default App;
---
+++ clientServer/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = new express();
+const path = require("path");
+const port = 8000;
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+});
