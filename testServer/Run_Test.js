a7d58cd2db08e7af001309ec97b3476878e0f7e2
bryan <orangesoupjacket@gmail.com>
2019-02-06 14:59:35 -0500

    ws integration without package-lock

--- testServer/Run_Test.js
+++ testServer/Run_Test.js
@@ -3,16 +3,23 @@ const Jasmine = require('jasmine');
 const jasmine = new Jasmine();
 
 jasmine.loadConfig({
-  spec_dir: 'spec'
-})
+  spec_dir: 'spec',
+  spec_fiels: ['Run_Test.js']
+});
 
 function mockTest(x) {
   return x * 2;
 }
 
+describe('test', () => {
+  it('testing mock', () => {
+    expect(mockTest(2)).toBe(4)
+  })
+})
+
 const test = {
   runTest: (req, res, next) => {
-    
+    jasmine.execute();
   }
 };
 
